dd376abdeea68834a960a1744a180308ad6e4e8d
Profile Name Here <fabian.rueda14@gmail.com>
2023-05-29 21:15:14 -0500

    Update streamlit_app.py
    Adding new import

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # shows the normalized json data into a pandas dataframe
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
